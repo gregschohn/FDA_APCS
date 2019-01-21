28edbe33b11b840363c1ba5ca00440df4bac9fee
Greg Schohn <greg.schohn@gmail.com>
2019-01-21 17:11:01 -0500

    Cleanup assertions to move the descriptions into the test names.

--- WordGolf/src/ParseSentenceTest.java
+++ WordGolf/src/ParseSentenceTest.java
@@ -3,19 +3,19 @@ import static org.junit.Assert.*;
 
 public class ParseSentenceTest {
 
-    @Test public void parseSentence1() {
-        assertEquals("hello world", 19, Main.parseSentence("hello world"));
+    @Test public void hello_world() {
+        assertEquals(19, Main.parseSentence("hello world"));
     }
-    @Test public void parseSentence2() {
-        assertEquals("the owls are not what they seem", 45, Main.parseSentence("the owls are not what they seem"));
+    @Test public void the_owls_are_not_what_they_seem() {
+        assertEquals(45, Main.parseSentence("the owls are not what they seem"));
     }
-    @Test public void parseSentence3() {
-        assertEquals("i love computer science", 67, Main.parseSentence("i love computer science"));
+    @Test public void i_love_computer_science() {
+        assertEquals(67, Main.parseSentence("i love computer science"));
     }
-    @Test public void parseSentence4() {
-        assertEquals("but not apple", 20, Main.parseSentence("but not apple"));
+    @Test public void but_not_apple() {
+        assertEquals(20, Main.parseSentence("but not apple"));
     }
-    @Test public void parseSentence5() {
-        assertEquals("help", 6, Main.parseSentence("help"));
+    @Test public void help() {
+        assertEquals(6, Main.parseSentence("help"));
     }
 }
--- WordGolf/src/ParseWordTest.java
+++ WordGolf/src/ParseWordTest.java
@@ -3,29 +3,29 @@ import static org.junit.Assert.*;
 
 public class ParseWordTest {
 
-    @Test public void parseWord1() {
-        assertEquals("Parsing the word 'java' returned incorrect number", 10, Main.parseWord("java"));
+    @Test public void java() {
+        assertEquals(10, Main.parseWord("java"));
     }
-    @Test public void parseWord3() {
-        assertEquals("Parsing the word 'google' returned incorrect number", 20, Main.parseWord("google"));
+    @Test public void google() {
+        assertEquals(20, Main.parseWord("google"));
     }
-    @Test public void parseWord4() {
-        assertEquals("Parsing the word 'help' returned incorrect number", 6, Main.parseWord("help"));
+    @Test public void help() {
+        assertEquals(6, Main.parseWord("help"));
     }
-    @Test public void parseWord5() {
-        assertEquals("Parsing the word 'apcs' returned incorrect number", 5, Main.parseWord("apcs"));
+    @Test public void apcs() {
+        assertEquals(5, Main.parseWord("apcs"));
     }
-    @Test public void parseWord6() {
-        assertEquals("Parsing the word 'testing' returned incorrect number", 14, Main.parseWord("testing"));
+    @Test public void testing() {
+        assertEquals(14, Main.parseWord("testing"));
     }
-    @Test public void parseWord7() {
-        assertEquals("Parsing the word 'computer' returned incorrect number", 27, Main.parseWord("computer"));
+    @Test public void computer() {
+        assertEquals(27, Main.parseWord("computer"));
     }
-    @Test public void parseWord8() {
-        assertEquals("Parsing the word 'science' returned incorrect number", 28, Main.parseWord("science"));
+    @Test public void science() {
+        assertEquals(28, Main.parseWord("science"));
     }
-    @Test public void parseWord9() {
-        assertEquals("Parsing the word 'rumpelstiltskin' returned incorrect number", 69, Main.parseWord("rumpelstiltskin"));
+    @Test public void rumpelstiltskin() {
+        assertEquals(69, Main.parseWord("rumpelstiltskin"));
     }
 
 }
